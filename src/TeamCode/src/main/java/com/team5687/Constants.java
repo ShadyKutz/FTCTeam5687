a28166fa5ecb8fe4d11fc0dc42e433d788349b0f
Stephen Post <stephen.post@ge.com>
2016-10-23 14:03:47 -0700

    Add Light Sensor OpMode for Testing

--- src/TeamCode/src/main/java/com/team5687/Constants.java
+++ src/TeamCode/src/main/java/com/team5687/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
     public static String RIGHT_DRIVE_MOTOR = "right_drive";
 
     public static String FRONT_RANGE_SENSOR = "sensor_range";
+
+    public static String LIGHT_SENSOR = "light_sensor";
 }
