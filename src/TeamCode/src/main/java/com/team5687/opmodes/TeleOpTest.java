fafce5d6cf82ba6a89179433025657550bb8878d
Stephen Post <stephen.post@ge.com>
2016-11-03 16:09:39 -0700

    Added spinner controller and joystick controller using gamepad

---
+++ src/TeamCode/src/main/java/com/team5687/opmodes/TeleOpTest.java
@@ -0,0 +1,40 @@
+package com.team5687.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.team5687.controllers.JoystickController;
+import com.team5687.controllers.SpinnerController;
+import com.team5687.helpers.Logger;
+
+/**
+ * Created by RedDragon on 10/25/2016.
+ */
+
+
+
+@TeleOp(name = "TeleOptest", group = "Test")
+public class TeleOpTest extends OpMode {
+
+    public float Power1 =100;
+    JoystickController _joy = new JoystickController();
+    SpinnerController _spin = new SpinnerController();
+
+
+    @Override
+    public void init() {
+        telemetry.addLine("Init()");
+        Logger.getInstance().SetTelemetry(telemetry);
+        Logger.getInstance().WriteMessage("TeleOpTest::Init()");
+        _joy = new JoystickController();
+        _joy.Init(hardwareMap, gamepad1);
+        _spin.Init(hardwareMap, gamepad1);
+
+        Logger.getInstance().WriteMessage(gamepad1 != null ? "Not Null" : "null");
+    }
+
+    @Override
+    public void loop() {
+        _joy.Loop();
+        _spin.Loop();
+    }
+}
